2837cea5ab5845cffd85f53afd105ba7c47d831f
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-03-28 11:32:40 +0900

    Initial Commit

---
+++ Alien ImageViewer Timer03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien ImageViewer Timer03
+//
+//  Created by D7702_09 on 2019. 3. 28..
+//  Copyright © 2019년 pgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
